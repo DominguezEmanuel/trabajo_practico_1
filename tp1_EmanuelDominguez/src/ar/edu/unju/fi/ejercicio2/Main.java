b8830d0f4ab9ac716e55ae40c574b0fd70094c38
HERMANOS <HERMANOS@DESKTOP-10IQNUD>
2024-04-06 10:53:03 -0300

    Se agrega paquete y clase para el ejercicio 2

---
+++ tp1_EmanuelDominguez/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,20 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String nombrepais="Argentina", number="+3885011475";
+		int edad=22;
+		Double altura=150.5, coseno=Math.cos(0.5);
+		Float precio=10.99f;
+		
+		//Salida por pantalla
+		System.out.println("Nombre del país: "+nombrepais);
+		System.out.println("Edad de una persona: "+edad+" años");
+		System.out.println("Altura de un edificio: "+altura+"mts");
+		System.out.println("Precio de un producto: $"+precio);
+		System.out.println("Número telefónico: "+number);
+		System.out.println("Cálculo de coseno: "+coseno);
+	}
+}
