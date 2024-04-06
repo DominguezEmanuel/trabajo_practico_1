b3ca292f7b9e4996bfee7a6d58b52e9c750d8f39
HERMANOS <HERMANOS@DESKTOP-10IQNUD>
2024-04-05 21:32:38 -0300

    Se agrega paquete y clase para el ejercicio 1

---
+++ tp1_EmanuelDominguez/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,17 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//Uso de variables
+		
+		int a=8, b=5, c=1, d=20, e=9;
+		int suma=a+b+c+d+e;
+		Float promedio;
+		promedio=suma/5.0f;
+
+		System.out.println("El promedio es "+promedio);
+	}
+
+}
